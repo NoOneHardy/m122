b3aeec59e9fd0ac13b600d99f92312ada9bc05ac
NoOneHardy <silas.hardegger@outlook.com>
2023-04-15 23:04:44 +0200

    Update praxisaufgabe.ps1

--- praxisaufgabe.ps1
+++ praxisaufgabe.ps1
@@ -3,16 +3,20 @@ $names = @{}
 
 while ($true) {
     # Eingabe
-    $input = Read-Host "Bitte gib einen Namen ein: "
+    $input = Read-Host "Bitte gib einen Namen ein"
     # Überprüfen ob die Eingabe exit ist
     if ($input -eq "exit") {
         exit
     }
 
     # Vorname und Nachname trennen
-    $firstname = $input.Split(" ")[0]
-    $name = $input.Split(" ")[$input.Split.Length - 1]
+    $inputNames = $input.Split(" ")
+    # Vorname speichern
+    $firstname = $inputNames[0]
+    # Nachname speichern
+    $name = $inputNames[$inputNames.Length - 1]
     $names[$firstname] = $name
 
+    # Hashtable ausgeben
     $names
 }
